09d861972d61b3bbfc7186d4b7d2d17ccf6edea0
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-05-13 19:56:14 +0200

    feat: introduce an iterator based on an index at the store

---
+++ src/store/iter.rs
@@ -0,0 +1,48 @@
+use redb::{AccessGuard, ReadOnlyTable};
+use crate::app::download::RepositoryError;
+use crate::types::Download;
+
+pub struct RedbDownloadIndexIter {
+    downloads: ReadOnlyTable<&'static str, &'static str>,
+    index: RedbIndexIter,
+}
+
+pub type RedbIndexEntry = redb::Result<(
+    AccessGuard<'static, &'static str>,
+    AccessGuard<'static, &'static str>,
+)>;
+
+pub type RedbIndexIter = Box<dyn Iterator<Item = RedbIndexEntry>>;
+
+impl RedbDownloadIndexIter {
+    pub fn new(downloads: ReadOnlyTable<&'static str, &'static str>, index: RedbIndexIter) -> Self {
+        Self { downloads, index }
+    }
+}
+
+impl Iterator for RedbDownloadIndexIter {
+    type Item = Result<Download, RepositoryError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next_entry = self.index.next()?;
+
+        Some(match next_entry {
+            Ok((_, info_hash)) => {
+                let info_hash = info_hash.value();
+                match self
+                    .downloads
+                    .get(info_hash)
+                    .map_err(|e| RepositoryError::Backend(e.to_string()))
+                {
+                    Ok(Some(download)) => serde_json::from_str(download.value())
+                        .map_err(|e| RepositoryError::Serialization(e.to_string())),
+                    Ok(None) => Err(RepositoryError::Backend(format!(
+                        "dangling download index entry for {info_hash}"
+                    ))),
+                    Err(e) => Err(e),
+                }
+            }
+            Err(e) => Err(RepositoryError::Backend(e.to_string())),
+        })
+    }
+}
