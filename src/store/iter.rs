9e1010ab3a598360b233ff4445cdd567e7e96288
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-07-29 10:11:13 +0200

    feat: impl collette iterator multiplexer

--- src/store/iter.rs
+++ src/store/iter.rs
@@ -1,6 +1,24 @@
+use collette::backend::redb::{RedbMultiStore, RedbReadStore};
+use collette::iter::{CollectionIterator, Entry, IndexIterator};
 use crate::app::download::RepositoryError;
 use crate::types::Download;
 use redb::{AccessGuard, ReadOnlyTable};
+
+pub enum DownloadIter {
+    IndexScan(IndexIterator<RedbReadStore, Download>),
+    ColScan(CollectionIterator<RedbReadStore, Download>),
+}
+
+impl Iterator for DownloadIter {
+    type Item = Result<Entry<Download>, RepositoryError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            DownloadIter::IndexScan(iter) => iter.next(),
+            DownloadIter::ColScan(iter) => iter.next(),
+        }.map(|res| res.map_err(Into::into))
+    }
+}
 
 pub struct RedbDownloadIndexIter {
     downloads: ReadOnlyTable<&'static str, &'static str>,
